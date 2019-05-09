5bdad3acb6092fa0603554e702e52f7381e83586
Elizaveta Dobrynina <dea998@gmail.com>
2019-05-09 16:36:45 +0300

    изменена кнопка "добавить расход" на floatingactionbutton

--- Project/MyCosts2/app/src/main/java/com/example/mycosts/MainActivity.java
+++ Project/MyCosts2/app/src/main/java/com/example/mycosts/MainActivity.java
@@ -1,38 +1,41 @@
 package com.example.mycosts;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-    System system = new System();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(".NewExpenseActivity");
+                startActivity(intent);
+            }
+        });
+
+
+
 
     }
 
-    public void newExpenseClick(View v){
-        Intent intent = new Intent(".NewExpenseActivity");
-        startActivity(intent);
-}
+//    public void newExpenseClick(View v){
+//        Intent intent = new Intent(".NewExpenseActivity");
+//        startActivity(intent);
+//}
 
     public void reportClick(View v){
         Intent intent = new Intent(".reportActivity");
@@ -43,10 +46,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(".allCategoriesActivity");
         startActivity(intent);
     }
-    public void allExpensesClick(View v){
-        Intent intent = new Intent(".allExpensesActivity");
-        startActivity(intent);
-    }
-
 
 }
