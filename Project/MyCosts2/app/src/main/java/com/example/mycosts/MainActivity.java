0ebdf70c24e0e51d858a4902ed5b33d0718f6049
Elizaveta Dobrynina <dea998@gmail.com>
2019-05-11 17:58:43 +0300

    добавлена новая иконка приложения, вместо кнопок добавлено нижнее меню

--- Project/MyCosts2/app/src/main/java/com/example/mycosts/MainActivity.java
+++ Project/MyCosts2/app/src/main/java/com/example/mycosts/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.mycosts;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.MenuItem;
 import android.view.View;
 
 
@@ -27,24 +30,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()){
+                        case R.id.category:
+                            Intent intent = new Intent(".allCategoriesActivity");
+                            startActivity(intent);
+                            break;
+                        case R.id.report:
+                            Intent intent2 = new Intent(".reportActivity");
+                            startActivity(intent2);
+                            break;
+                    }
+                    return false;
+                }
+            });
 
     }
 
-//    public void newExpenseClick(View v){
-//        Intent intent = new Intent(".NewExpenseActivity");
+
+
+//    public void reportClick(View v){
+//        Intent intent = new Intent(".reportActivity");
 //        startActivity(intent);
-//}
-
-    public void reportClick(View v){
-        Intent intent = new Intent(".reportActivity");
-        startActivity(intent);
-    }
-
-    public void allCategoriesClick(View v){
-        Intent intent = new Intent(".allCategoriesActivity");
-        startActivity(intent);
-    }
+//    }
+//
+//    public void allCategoriesClick(View v){
+//        Intent intent = new Intent(".allCategoriesActivity");
+//        startActivity(intent);
+//    }
 
 }
